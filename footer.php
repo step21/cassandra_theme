5608b07a2503ea8cee41cf92d56c97e1e2d3fee3
step21 <step21@devtal.de>
2021-07-25 22:23:56 +0200

    outcomment WP footer

--- footer.php
+++ footer.php
@@ -13,16 +13,16 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cassandra' ) ); ?>">
+		<?php /*	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cassandra' ) ); ?>"> */ ?>
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'cassandra' ), 'WordPress' );
+				/* printf( esc_html__( 'Proudly powered by %s', 'cassandra' ), 'WordPress' ); */
 				?>
-			</a>
-			<span class="sep"> | </span>
+		<?php /*	</a>
+			<span class="sep"> | </span> */ ?>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'cassandra' ), 'cassandra', '<a href="https://github.com/step21">Florian</a>' );
+				/* printf( esc_html__( 'Theme: %1$s by %2$s.', 'cassandra' ), 'cassandra', '<a href="https://github.com/step21">Florian</a>' ); */
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
